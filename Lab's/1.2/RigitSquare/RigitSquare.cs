4faf182166aae3a9bb327bf3553a9e07f8e3ed40
NemoNology <chicken.gang@mail.ru>
2023-02-20 03:33:51 +0900

    Created files

---
+++ Lab's/1.2/RigitSquare/RigitSquare.cs
@@ -0,0 +1,30 @@
+﻿using RigitBody2D;
+
+namespace RigitSquare;
+{
+    public class RigitSquare : IRigidBody2D
+    {
+        public Point Coordinates { get; set; } = 0;
+
+        public double SpeedX { get; set; } = 0;
+
+        public double AccelerationX { get; set; } = 0;
+
+        public double SpeedY { get; set; } = 0;
+
+        public double AccelerationY { get; set; } = 0;
+
+        public void MovementVectorByNewCoordinates(Point point)
+        {
+            SpeedX = point.X - Coordinates.X;
+            SpeedY = point.Y - Coordinates.Y;
+        }
+
+        public void Move()
+        {
+            
+        }
+    }
+
+}
+
