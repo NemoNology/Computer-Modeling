c18600027666673bb61246a62681b6482ff94541
NemoNology <chicken.gang@mail.ru>
2023-02-20 04:11:53 +0900

    Added RigitSquare class

--- Lab's/1.2/RigitSquare/RigitSquare.cs
+++ Lab's/1.2/RigitSquare/RigitSquare.cs
@@ -1,17 +1,37 @@
 ﻿using RigitBody2D;
 
-namespace RigitSquare;
+namespace RigitSquare
 {
     public class RigitSquare : IRigidBody2D
     {
+        /// <summary>
+        /// Side Legth
+        /// </summary>
+        public byte Size { get; set; }
+
+        /// <summary>
+        /// Square Center Coordinates
+        /// </summary>
         public Point Coordinates { get; set; } = 0;
 
+        /// <summary>
+        /// Speed by X - How change X Coordinate by time
+        /// </summary>
         public double SpeedX { get; set; } = 0;
 
+        /// <summary>
+        /// Acceleratio by X - How change Speed X by time
+        /// </summary>
         public double AccelerationX { get; set; } = 0;
 
+        /// <summary>
+        /// Speed by Y - How change Y Coordinate by time
+        /// </summary>
         public double SpeedY { get; set; } = 0;
 
+        /// <summary>
+        /// Acceleratio by Y - How change Speed Y by time
+        /// </summary>
         public double AccelerationY { get; set; } = 0;
 
         public void MovementVectorByNewCoordinates(Point point)
@@ -22,7 +42,11 @@ namespace RigitSquare;
 
         public void Move()
         {
-            
+            Coordinates.X += SpeedX;
+            Coordinates.Y += SpeedY;
+
+            SpeedX += AccelerationX;
+            SpeedY += AccelerationY;
         }
     }
 
