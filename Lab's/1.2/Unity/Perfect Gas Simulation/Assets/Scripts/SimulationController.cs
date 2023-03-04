9e2335f2d97d6e0c22718b833fbcf45c0fc53346
NemoNology <chicken.gang@mail.ru>
2023-03-04 23:45:50 +0900

    Added Status prop & LetsMove event

--- Lab's/1.2/Unity/Perfect Gas Simulation/Assets/Scripts/SimulationController.cs
+++ Lab's/1.2/Unity/Perfect Gas Simulation/Assets/Scripts/SimulationController.cs
@@ -1,12 +1,23 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.Audio;
+using UnityEngine.UI;
 
-public class SimulationSetting : MonoBehaviour
+public class SimulationController : MonoBehaviour
 {
     public float TimeMultiplier { get; set; }
     public int AtomsAmount { get; set; }
     public int AtomsSize { get; set; }
+    public string Status { get; }
+
+    public delegate void Move();
+    public event Move LetsMove;
     
     
+    private void Update()
+    {
+        
+    }
 }
