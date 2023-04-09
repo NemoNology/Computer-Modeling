7723c16201497c938587e31dd715e443373cfb2b
NemoNology <chicken.gang@mail.ru>
2023-04-10 06:39:40 +0900

    Build 0.75

--- Lab-s/4/Godot/ball.gd
+++ Lab-s/4/Godot/ball.gd
@@ -3,43 +3,79 @@ extends CharacterBody2D
 const g = 9.8;
 
 var _stringLength = 0.2;
-var _startAngleDegree = 30;
-var _startSpeed = 5;
+var _startAngleRad = deg_to_rad(30);
+var _speed = deg_to_rad(5);
 
 var frequency: float;
 var amplituda: float;
 var startPhase: float;
 
-var isSimulationProcessing: bool = false;
+var _isSimulationProcessing: bool = false;
+var time = 0;
+
 
 
 func _on_ready():
 	
-	
-	Recalculate();
 	position.y = _stringLength * 3e2;
-	Rotate(_startAngleDegree);
-	print(position)
-	
-	
+	position.x = 0;
+	Rotate(_startAngleRad);
+	Recalculate();
+
 
 func _physics_process(delta):
 	
-	if (!isSimulationProcessing):
+	if (!_isSimulationProcessing):
 		return;
-		
 	
-	
+	_speed = -(g / _stringLength) * sin(Solution(time));
+	Rotate(_speed * delta**2);
+	time += delta;
 	
 	
 
 func Recalculate():
 	
 	frequency = sqrt(_stringLength / g);
-	startPhase = atan(-deg_to_rad(_startSpeed) / (_stringLength * frequency * deg_to_rad(_startAngleDegree)));
-	amplituda = sqrt(deg_to_rad(_startAngleDegree)**2 + deg_to_rad(_startSpeed)**2 / (_stringLength * frequency)**2);
+	startPhase = atan(-_speed / (_stringLength * frequency * _startAngleRad));
+	amplituda = sqrt(_startAngleRad**2 + _speed**2 / (_stringLength * frequency)**2);
 
 
-func Rotate(angleDegree: float):
+func Solution(t: float):
 	
-	position = position.rotated(deg_to_rad(angleDegree));
+	return amplituda * cos(frequency * t + startPhase);
+
+func Rotate(angleRad: float):
+	
+	position.y = _stringLength * 3e2;
+	position.x = 0;
+	position = position.rotated(angleRad);
+
+
+func _on_input_string_length_value_changed(value):
+
+	_stringLength = value;
+	_on_stop_pressed();
+
+
+func _on_input_start_angle_value_changed(value):
+	
+	_startAngleRad = deg_to_rad(value);
+	_on_stop_pressed();
+
+
+func _on_input_start_speed_value_changed(value):
+	
+	_speed = deg_to_rad(value);
+	_on_stop_pressed();
+
+
+func _on_start_pressed():
+	
+	_isSimulationProcessing = true;
+
+
+func _on_stop_pressed():
+	
+	_isSimulationProcessing = false;
+	Recalculate();
