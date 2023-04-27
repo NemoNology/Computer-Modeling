e2df978370831d80866bda63c4d94d3745547cd8
NemoNology <chicken.gang@mail.ru>
2023-04-28 06:12:06 +0900

    Build 0.4 - only timer completed

---
+++ Lab-s/10/2-3/Project/ServiceSystem.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Project
+{
+    internal class ServiceSystem
+    {
+        public event Action<TimeSpan>? OnClientInputStart;
+        public event Action<TimeSpan>? OnClientDenied;
+        public event Action<TimeSpan>? OnClientPutInQueue;
+        public event Action<TimeSpan>? OnClientServed;
+        public event Action? OnServiceCompleted;
+
+        public int ServiceTime { get; set; } = 20;
+
+        private double Lambda(double time)
+        {
+            return 1.1 - Math.Pow(time - 50, 2) / 2500;
+        }
+
+        private double Nu(double time)
+        {
+            if (time <= 40)
+            {
+                return 0.7 * time;
+            }
+            else
+            {
+                return 0.9;
+            }
+        }
+
+        public async void ServiceShift(double timeMultiplier)
+        {
+            int Tn = ServiceTime;
+            double t = 0;
+
+            var rnd = new Random(DateTime.Now.Millisecond);
+
+            while (t <= Tn)
+            {
+                var buffer = Lambda(t);
+
+                var ro = -1 / buffer * Math.Log(rnd.NextDouble());
+
+                var timeDelay = TimeSpan.FromSeconds(ro / timeMultiplier);
+
+                t += ro;
+
+                OnClientInputStart?.Invoke(timeDelay);
+                await Task.Delay(timeDelay);
+            }
+
+            OnServiceCompleted?.Invoke();
+        }
+
+
+    }
+}
